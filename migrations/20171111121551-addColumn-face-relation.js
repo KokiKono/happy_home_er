ff265253bf18284269003aa77988c2e93573d1df
kokikono <kk.k.0421@outlook.jp>
2017-11-11 21:17:00 +0900

    addColumn: face_relation in image_path, json_data

---
+++ migrations/20171111121551-addColumn-face-relation.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var dbm;
+var type;
+var seed;
+
+/**
+  * We receive the dbmigrate dependency from dbmigrate initially.
+  * This enables us to not have to rely on NODE_PATH.
+  */
+exports.setup = function(options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+};
+
+exports.up = function(db) {
+  return db.addColumn('face_relation', 'json_data', {
+      type: type.TEXT
+  });
+};
+
+exports.down = function(db) {
+  return db.removeColumn('face_relation', 'json_data');
+};
+
+exports._meta = {
+  "version": 1
+};
